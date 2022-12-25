b77484418c30d1cd176f2b2ca4626c2390ef6b55
masayan382 <masan3821031@gmail.com>
2022-12-25 10:56:07 +0900

    pusher

---
+++ pusher.ts
@@ -0,0 +1,15 @@
+import Pusher from "pusher"
+import ClientPusher from "pusher-js"
+
+export const serverPusher: Pusher = new Pusher({
+    appId: `${process.env.PUSHER_ID}`,
+    key: `${process.env.PUSHER_KEY}`,
+    secret: `${process.env.PUSHER_SECRET}`,
+    cluster: `${process.env.PUSHER_CLUSTER}`,
+    useTLS: true,
+})
+
+export const clientPusher = new ClientPusher(`${process.env.PUSHER_KEY}`, {
+    cluster: `${process.env.PUSHER_CLUSTER}`,
+    forceTLS: true,
+})
