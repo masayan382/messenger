32a9c8dc6bc6d223e9a971f37363b6fb3251a79b
masayan382 <masan3821031@gmail.com>
2022-12-25 12:25:38 +0900

    環境変数修正

--- pusher.ts
+++ pusher.ts
@@ -2,14 +2,14 @@ import Pusher from "pusher"
 import ClientPusher from "pusher-js"
 
 export const serverPusher: Pusher = new Pusher({
-    appId: `${process.env.PUSHER_ID}`,
-    key: `${process.env.PUSHER_KEY}`,
-    secret: `${process.env.PUSHER_SECRET}`,
-    cluster: `${process.env.PUSHER_CLUSTER}`,
+    appId: `${process.env.NEXT_PUBLIC_PUSHER_ID}`,
+    key: `${process.env.NEXT_PUBLIC_PUSHER_KEY}`,
+    secret: `${process.env.NEXT_PUBLIC_PUSHER_SECRET}`,
+    cluster: `${process.env.NEXT_PUBLIC_PUSHER_CLUSTER}`,
     useTLS: true,
 })
 
 export const clientPusher = new ClientPusher(`${process.env.PUSHER_KEY}`, {
-    cluster: `${process.env.PUSHER_CLUSTER}`,
+    cluster: `${process.env.NEXT_PUBLIC_PUSHER_CLUSTER}`,
     forceTLS: true,
 })
